0c77cb696e0f48f7f434ad2cbfb70f74c7485269
liyang <2363582258@qq.com>
2020-08-20 16:59:04 +0800

    swaggui的优化

--- src/main/java/com/yc/projects/yc74ibike/config/AppConfig_Swagger.java
+++ src/main/java/com/yc/projects/yc74ibike/config/AppConfig_Swagger.java
@@ -1,7 +1,9 @@
 package com.yc.projects.yc74ibike.config;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
@@ -15,30 +17,29 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@ComponentScan(basePackages="com.yc")
 @EnableSwagger2    //启用swaggerr 注解解析器
 public class AppConfig_Swagger {
 	// 是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
 	@Value(value = "${swagger.enabled}")  //通过 @Value  获取配置信息   复习   @Environement  @Value    @ConfigurationProperties
 	Boolean swaggerEnabled;
-	
-	@Bean
+    @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
-                // 是否开启
-                .enable(swaggerEnabled).select()
-                // 扫描的路径包,只要这些包中的类配有swagger注解，则启用这些注解
-                .apis(RequestHandlerSelectors.basePackage("com.yc"))
-                // 指定路径处理PathSelectors.any()代表所有的路径
-                .paths(     PathSelectors.any()     ).build().pathMapping("/");
-    }
-	//swagger首页信息
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("小辰出行操作接口")
-                .description("springboot | swagger")
-                // 作者信息
-                .contact(  new Contact("zy", "http://www.hyycinfo.com", "1069595532@qq.com")  )
-                .version("1.0.0")
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zx.qcdzh.web"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("项目管理系统 RESTful APIs")
+                .description("项目管理系统接口（API）使用文档")
+                .version("1.0")
+                .build();
+    }
+
 }
